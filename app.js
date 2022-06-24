c7db3dd2acf259dc277e18f52c2c0339f0531819
emiliano <emicabanillas.cab@gmail.com>
2022-06-23 22:52:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,17 +15,20 @@ class Pokemon{
             // console.log(this.stats["ps"]);
         }
     }
+    
 }
-//Pikachu
-const pikachu = new Pokemon("Pikachu", {ps: 274, atk: 229, def: 196, atk_esp: 199, def_esp: 199, vel: 279}, "Electrico", [{"Impactrueno": 40,"Tipo": "Electrico", "Categoria": "Especial"}, {"Chispa": 65, "Tipo": "Electrico", "Categoria": "Fisico"}, {"Ataque Rapido": 40, "Tipo": "Normal", "Categoria": "Fisico"}, {"Gruñido": 0.95, "Tipo": "Normal", "Categoria": "Estado"}], {"Cantitad": 2, "Cura": 25});
-//Squirtle
-const squirtle = new Pokemon("Squirtle", {ps: 292, atk: 214, def: 251, atk_esp: 199, def_esp: 227, vel: 185}, "Agua", [{"Placaje": 40, "Tipo": "Normal", "Categoria": "Fisico"}, {"Acua Cola": 90, "Tipo": "Agua", "Categoria": "Fisico"}, {"Burbuja": 40, "Tipo": "Agua", "Categoria": "Especial"}, {"Defensa Ferrea": 1.10, "Tipo": "Acero", "Categoria": "Estado"}], {"Cantitad": 2, "Cura": 25});
-//Charmander
-const charmander = new Pokemon("Charmander", {ps: 282, atk: 203, def: 185, atk_esp: 219, def_esp: 199, vel: 229}, "Fuego",[{"Garra Metal": 40, "Tipo": "Acero", "Categoria": "Fisico"},{"Colmillo Igneo": 65, "Tipo": "Fuego", "Categoria": "Fisico"},{"Lanzallamas": 90, "Tipo": "Fuego", "Categoria": "Especial"}, {"Malicioso": 0.95, "Tipo": "Normal", "Categoria": "Estado"}], {"Cantitad": 2, "Cura": 25})
-//Bulbasaur
-const bulbasour = new Pokemon("Bulbasaur", {ps: 294, atk: 197, def: 197, atk_esp: 229, def_esp: 229, vel: 207}, "Planta", [{"Drenadoras": 0.0625, "Tipo": "Planta", "Categoria": "Estado"}, {"Latigo Cepa": 45, "Tipo": "Planta", "Categoria": "Fisico"}, {"Hoja Afilada": 55, "Tipo": "Planta", "Categoria": "Fisico"}, {"Rayo Solar": 90, "Tipo": "Planta", "Categoria": "Especial"}], {"Cantitad": 2, "Cura": 25})
 
-console.log(pikachu);
+
+//Pikachu
+const pikachu = new Pokemon("Pikachu", {ps: 274, atk: 229, def: 196, atk_esp: 199, def_esp: 199, vel: 279}, "Electrico", [{Poder: "Impactrueno", Potencia: 40,"Tipo": "Electrico", "Categoria": "Especial"}, {Poder: "Chispa", Potencia: 65, "Tipo": "Electrico", "Categoria": "Fisico"}, {Poder: "Ataque Rapido", Potencia: 40, "Tipo": "Normal", "Categoria": "Fisico"}, {Poder: "Gruñido", Potencia: 0.95, "Tipo": "Normal", "Categoria": "Estado"}], {"Cantidad": 2, "Cura": 25});
+//Squirtle
+const squirtle = new Pokemon("Squirtle", {ps: 292, atk: 214, def: 251, atk_esp: 199, def_esp: 227, vel: 185}, "Agua", [{Poder: "Placaje", Potencia: 40, "Tipo": "Normal", "Categoria": "Fisico"}, {Poder: "Acua Cola", Potencia: 90, "Tipo": "Agua", "Categoria": "Fisico"}, {Poder: "Burbuja", Potencia: 40, "Tipo": "Agua", "Categoria": "Especial"}, {Poder: "Defensa Ferrea", Potencia: 1.10, "Tipo": "Acero", "Categoria": "Estado"}], {"Cantidad": 2, "Cura": 25});
+//Charmander
+const charmander = new Pokemon("Charmander", {ps: 282, atk: 203, def: 185, atk_esp: 219, def_esp: 199, vel: 229}, "Fuego",[{Poder: "Garra Metal", Potencia: 40, "Tipo": "Acero", "Categoria": "Fisico"},{Poder: "Colmillo Igneo", Potencia: 65, "Tipo": "Fuego", "Categoria": "Fisico"},{Poder: "Lanzallamas", Potencia: 90, "Tipo": "Fuego", "Categoria": "Especial"}, {Poder: "Malicioso", Potencia: 0.95, "Tipo": "Normal", "Categoria": "Estado"}], {"Cantidad": 2, "Cura": 25})
+//Bulbasaur
+const bulbasour = new Pokemon("Bulbasaur", {ps: 294, atk: 197, def: 197, atk_esp: 229, def_esp: 229, vel: 207}, "Planta", [{Poder: "Drenadoras", Potencia: 0.0625, "Tipo": "Planta", "Categoria": "Estado"}, {Poder: "Latigo Cepa", Potencia: 45, "Tipo": "Planta", "Categoria": "Fisico"}, {Poder: "Hoja Afilada", Potencia: 55, "Tipo": "Planta", "Categoria": "Fisico"}, {Poder: "Rayo Solar", Potencia: 90, "Tipo": "Planta", "Categoria": "Especial"}], {"Cantidad": 2, "Cura": 25})
+
+console.log(pikachu.poderes[1].Potencia);
 
 function intro(){
     let name = prompt("Ingresa tu nombre de entrenador...")
@@ -33,7 +36,7 @@ function intro(){
     alert(`**********************************************\n BIENVENIDO ${nameUp} A LA BATALLA POKEMON \n **********************************************`);
     
     alert("Bueno, ahora te voy a dejar que elijas tu Pokemon");
-    let eleccion = prompt("Elije el Pokemon que quieras luchar: \n 1. Pikachu\n 2. Squirtle \n 3. Charmander \n 4. Bulbasour")
+    let eleccion = prompt("Elije el Pokemon que quieras luchar: \n 1. Pikachu\n 2. Squirtle \n 3. Charmander \n 4. Bulbasaur")
     
     
 }
